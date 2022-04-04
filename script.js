4c39470ab6cb72b29919bdf4708e7c68490c0268
Kosmas <kosmasamsok03@gmail.com>
2022-04-05 01:31:02 +0300

    Added message data

---
+++ script.js
@@ -0,0 +1,9 @@
+/*The goal of the project is to generate a randomised message each time for the user 
+My interpretation of the excersise is to make a program that prints out "Good morning "name". it was nice meeting you. Have a nice day."
+but each word will be randomly selected from an array of different languages , so that the final message will be a mix of languages . ex. "Bonjour "name". Χαρηκα για την γνωριμια,良い一日を過ごして"
+*/
+let goodMorningArray=["bonjour","buenos días","guten Morgen","buongiorno","bom dia","suprabhat","zǎoān"];
+let niceMeetArray=["Ce fut un plaisir de vous rencontrer","Fue un placer conocerte","Es war mit ein Vergnügen Sie kennen zu lernen","È stato un piacere conoscerti","Foi um prazer conhece-lo","aapase mil kar khushee huee","Hěn gāoxìng jiàn dào nǐ"];
+let goodDayArray=["Passe une bonne journée","Que tenga un buen día","Haben Sie einen guten Tag","Buona giornata","Tenha um bom dia","aapaka din shubh ho","Zhù nǐ yǒu měihǎo de yītiān"];
+
+console.log("Hello World");
